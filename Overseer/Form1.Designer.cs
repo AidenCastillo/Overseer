bde24930c9de3656ab73650e42ffa88f21ec7bff
Aiden Castillo <amc-mcjarjar@hotmail.com>
2021-04-18 00:56:52 -0500

    Add project files.

---
+++ Overseer/Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Overseer
+{
+    partial class Overseer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Overseer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Overseer";
+            this.Text = "Overseer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
